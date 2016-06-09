1d7d5bd8b1e11fed8ac62dcd3e9416f270273156
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-06-09 09:36:56 -0700

    got info windows to pop up

--- public/js/feedmarkers.js
+++ public/js/feedmarkers.js
@@ -20,11 +20,22 @@
           geocoder.geocode({'address': addresstext}, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
             resultsMap.setCenter(results[0].geometry.location);
+
+
+             var infowindow = new google.maps.InfoWindow({
+                content: "<p style='color: black'>blah</p>"
+             });
+
             var marker = new google.maps.Marker({
               map: resultsMap,
               position: results[0].geometry.location
-
             });
+
+             marker.addListener('click', function() {
+              infowindow.open(resultsMap, marker);
+            });
+
+
             //debugger;
           } else {
             alert('Geocode was not successful for the following reason: ' + status);
