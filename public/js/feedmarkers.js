4eb5dded69e18a68c07f29b2f58ddbe798329852
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-06-09 14:38:33 -0700

    finish newest page

--- public/js/feedmarkers.js
+++ public/js/feedmarkers.js
@@ -14,42 +14,37 @@
         var address = document.getElementsByClassName('paragraph');
         var userinfo = document.getElementsByClassName('userinfo');
 
+
+
         for (var i = 0; i <address.length; i ++){
-        //for (var i in address){
           addresstext = address[i].innerHTML
           userinfotext = userinfo[i].innerHTML
-          console.log(userinfotext);
-            var infowindow = new google.maps.InfoWindow({
-                content: "<p style='color: black'>" + userinfotext + "</p>"
 
-             });
+          var infowindow = new google.maps.InfoWindow({
+              content: "<p style='color: black'>" + userinfotext + "</p>"
+           });
+         console.log("Before the geocode callback")
+         console.log(userinfotext)
           geocoder.geocode({'address': addresstext}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+              resultsMap.setCenter(results[0].geometry.location);
+              console.log("Within the geocode callback")
+               console.log(userinfotext)
+              var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+              });
 
-          if (status === google.maps.GeocoderStatus.OK) {
-            resultsMap.setCenter(results[0].geometry.location);
-
-
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-
-
-
-             marker.addListener('click', function() {
-              infowindow.open(resultsMap, marker);
-
-
-            });
-
-
-
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
+              marker.addListener('click', function() {
+                infowindow.open(resultsMap, marker);
+              });
+            }
+            else {
+              alert('Geocode was not successful for the following reason: ' + status);
+            }
+          });
+        }
       }
-    }
 
 
 // debugger;
--- public/js/showmarker.js
+++ public/js/showmarker.js
@@ -13,15 +13,30 @@
       function geocodeAddress(geocoder, resultsMap) {
         //var address = document.getElementById('address').value;
         var address = document.getElementById('paragraph').innerHTML;
+        var username = document.getElementById('username').innerHTML;
+
+
+         var infowindow = new google.maps.InfoWindow({
+              content: "<p style='color: black'>" + username + "</p>"
+           });
+
 
         geocoder.geocode({'address': address}, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
             resultsMap.setCenter(results[0].geometry.location);
+
             var marker = new google.maps.Marker({
               map: resultsMap,
               position: results[0].geometry.location
-
             });
+
+            marker.addListener('click', function() {
+              infowindow.open(resultsMap, marker);
+            });
+
+
+
+
           } else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
