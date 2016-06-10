5499babcc70f051750d5ca2ec1ba0fce9ab0f85a
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-06-09 21:29:24 -0700

    fix styling for maps

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -2,5 +2,5 @@ post '/comments' do
   @current_user = User.find(session[:user_id]) if session[:user_id]
   @log = Log.find(params[:logid])
   comment = Comment.create(content: params[:content], user_id: @current_user.id, log_id: params[:logid] )
-  redirect "/logs/4"
+  redirect "/logs/#{@log.id}"
 end
