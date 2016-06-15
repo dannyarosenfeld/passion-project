09f6d15c561dc0f16b4c88ce1d1b75578edd5f59
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-06-14 21:45:44 -0700

    change indentation comment controller

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -2,8 +2,6 @@ post '/comments' do
   @current_user = User.find(session[:user_id]) if session[:user_id]
   @log = Log.find(params[:logid])
   comment = Comment.create(content: params[:content], user_id: @current_user.id, log_id: params[:logid] )
-
-
   if request.xhr?
     p "blah"*30
     @comments = Comment.all
